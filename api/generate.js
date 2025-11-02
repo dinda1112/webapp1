c66b94f8359b47d02e429fa45136804729669276
dinda1112 <153375496+dinda1112@users.noreply.github.com>
2025-11-02 22:32:39 +0800

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
     const fullPrompt = `Write a ${type} about: ${prompt}`;
 
   const response = await fetch(
-  "https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash-001:generateContent?key=" + API_KEY,
+  "https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash-latest:generateContent?key=" + API_KEY,
   {
     method: "POST",
     headers: { "Content-Type": "application/json" },
@@ -20,6 +20,7 @@ export default async function handler(req, res) {
     }),
   }
 );
+
 
 
 
